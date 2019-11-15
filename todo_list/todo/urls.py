54daef33c388ba23740319ccce0ca0c563d6fa8d
Placide IRANDORA <placideirandora@gmail.com>
2019-11-16 01:14:54 +0200

    Add a path for completing a specific todo from the server

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add', views.add_todo, name='add')
+    path('add', views.add_todo, name='add'),
+    path('complete/<todo_id>', views.complete_todo, name='complete')
 ]
