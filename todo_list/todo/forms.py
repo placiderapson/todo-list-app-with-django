f41a9d2271eb487c9667d931adc947781f33f340
Placide IRANDORA <placideirandora@gmail.com>
2019-11-16 00:32:16 +0200

    Specify the attributes for styling purpose

--- todo_list/todo/forms.py
+++ todo_list/todo/forms.py
@@ -1,5 +1,10 @@
 from django import  forms
 
 class TodoForm(forms.Form):
-    text = forms.CharField(max_length=30)
-    
+    text = forms.CharField(max_length=30,
+    widget=forms.TextInput(
+        attrs={'class': 'form-control', 
+        'placeholder': 'Enter todo e.g. Learn how to play guitar',
+        'aria-label': 'Todo', 
+        'aria-describedby':'add-btn'}
+    ))
