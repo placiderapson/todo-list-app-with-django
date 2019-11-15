c95241363c301ad610c23f92694bdfb4109a090d
Placide IRANDORA <placideirandora@gmail.com>
2019-11-16 00:19:08 +0200

    Pass the form to the template

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 
 def index(request):
     todo_list = Todo.objects.order_by('id')
-    context = {'todo_list': todo_list}
+    form = TodoForm()
+    context = {'todo_list': todo_list, 'form': form}
     return render(request, 'todo/index.html', context)
