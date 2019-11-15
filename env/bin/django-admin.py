e5a173094d8e7f1561764578766b3f06a146c762
Placide IRANDORA <placideirandora@gmail.com>
2019-11-15 20:20:44 +0200

    Install Django

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/placideirandora/Coding/todo-list-app-with-django/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
