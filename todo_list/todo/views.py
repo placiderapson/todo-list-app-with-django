f559e453d29ed19e5116ace6b6ef703f009e1369
Placide IRANDORA <placideirandora@gmail.com>
2019-11-16 00:54:11 +0200

    Improve the readability

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -8,7 +8,9 @@ from .forms import TodoForm
 def index(request):
     todo_list = Todo.objects.order_by('id')
     form = TodoForm()
+
     context = {'todo_list': todo_list, 'form': form}
+    
     return render(request, 'todo/index.html', context)
 
 @require_POST
