bfbcca2ff45fbb019d1535fb119a7eb963aef614
Placide IRANDORA <placideirandora@gmail.com>
2019-11-15 21:59:17 +0200

    Fix the issue: ModuleNotFoundError: No module named 'tododjango'

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'todo'
+    'todo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
