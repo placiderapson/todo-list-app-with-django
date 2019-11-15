13b86166015f4486fbb718929cf5f3e192441d49
Placide IRANDORA <placideirandora@gmail.com>
2019-11-16 01:38:15 +0200

    Create a path for deleting all todos from the server

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add', views.add_todo, name='add'),
     path('complete/<todo_id>', views.complete_todo, name='complete'),
-    path('delete-completed', views.delete_completed, name='delete-completed')
+    path('delete-completed', views.delete_completed, name='delete-completed'),
+    path('delete-all', views.delete_all, name='delete-all')
 ]
