b9ac4131ffd3fd701a3eed7842ed52cf12d6fe1f
Placide IRANDORA <placideirandora@gmail.com>
2019-11-15 23:41:07 +0200

    Query the todos from database and pass them to the template for rendering them

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 
 def index(request):
-    context = {}
+    todo_list = Todo.objects.order_by('id')
+    context = {'todo_list': todo_list}
     return render(request, 'todo/index.html', context)
