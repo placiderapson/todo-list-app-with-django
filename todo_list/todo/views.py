4134d9ffdea335fa3de3199ca8e7385005c08780
Placide IRANDORA <placideirandora@gmail.com>
2019-11-16 00:53:31 +0200

    Create a function for handling todo submission

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
 from .models import Todo
 from .forms import TodoForm
 
@@ -9,3 +10,13 @@ def index(request):
     form = TodoForm()
     context = {'todo_list': todo_list, 'form': form}
     return render(request, 'todo/index.html', context)
+
+@require_POST
+def add_todo(request):
+    form = TodoForm(request.POST)
+    
+    if form.is_valid():
+        new_todo = Todo(text=request.POST['text'])
+        new_todo.save()
+
+    return redirect('index')
